4fb9d511acf4d5c6c6635d1bcc03f2cf9ed2abc6
Grzegorz Nowak <grzegorz.nowak@pl.abb.com>
2017-09-01 15:54:35 +0200

    f

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 http.createServer(function(req, res)
 {
     res.writeHead(200, {'content-type' : 'text/html'});
-    var html = fs.readFileSync('./index.html');
+    var html = fs.readFileSync('./index.html', 'utf-8');
+    html = html.replace('{ Header }', 'nowakgg');
     res.end(html);
 }).listen(3000);
 
